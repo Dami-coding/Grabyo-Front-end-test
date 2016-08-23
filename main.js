3c69a87928609669d32df626a31ab5d0e7c8aa01
dami <boldsdee@aol.com>
2016-08-23 14:55:10 +0100

    Added code to github

---
+++ main.js
@@ -0,0 +1,12 @@
+// $(function(){
+
+//   $('#chart li').each(function(){
+//     var pc = $(this).attr('title');
+//     pc = pc > 100 ? 100 
+//     $(this).children('.parent').html(pc+'%');
+//     var ww = $(this).width();
+//     var len = parseInt(ww, 10) * parseInt(pc, 10) / 100
+//     $(this).children('.bar').animate({ 'width' : len + 'px'}, 1500);
+
+//   })
+// })
